ca4855daaa09703365e40f770fb6d94a0e6a1895
allan <allan76440@gmail.com>
2021-02-10 10:51:33 +0100

    Html et Css

--- public/home/home.php
+++ public/home/home.php
@@ -1,12 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+<link rel="stylesheet" href="home.css">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Accueil - StreetShop</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../website_part/header.css">
+    <title>Inscription - StreetShop</title>
 </head>
+
 <body>
+    <?php 
+    
+    require "../../website_part/header.php";
+
+    ?>
+    <div id=fond>
+
+    <h1 class="pm-text">Produits</h1>
+    
+    </div>
+
 
 </body>
 </html>
