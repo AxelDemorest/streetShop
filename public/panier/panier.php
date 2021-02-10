0f8630b25d06538cb5c893b1c73fa53655f0d4c6
tristanlaine <tristan.laine711@gmail.com>
2021-02-10 17:22:31 +0100

    Panier affichage

---
+++ public/panier/panier.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+require_once '../../database/database.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../website_part/header.css">
+    <title>Panier - StreetShop</title>
+</head>
+
+<body>
+    <?php 
+    //require "../../website_part/header.php";
+    echo 'test';
+    print_r($_SESSION['panier']);
+    if (isset($_SESSION['panier'])){
+
+    echo 'test';
+    $tablPanier = $_SESSION['panier'];
+    $taille = count($tablPanier);
+    for ($i=0; $i < $taille; $i++){
+    echo $tablPanier[$i];
+    }
+    }
+    ?>
+
+
+</body>
+</html>
+
+
